41509c4e3420944f99fda0f9672185023fd8dd07
Nguyễn Thế Vinh <81474434+nguyenthevinhmt@users.noreply.github.com>
2024-04-01 22:48:14 +0700

    up

--- web-socket/index.js
+++ web-socket/index.js
@@ -1,39 +1,40 @@
 const io = require("socket.io")(8800, {
-    cors: {
-        origin: "*",
-    },
+  cors: {
+    origin: "*",
+  },
 });
 
 let activeUsers = [];
 
 io.on("connection", (socket) => {
-    // add new User
-    socket.on("new-user-add", (newUserId) => {
-        // if user is not added previously
-        if (!activeUsers.some((user) => user.userId === newUserId)) {
-            activeUsers.push({ userId: newUserId, socketId: socket.id });
-            console.log("New User Connected", activeUsers);
-        }
-        // send all active users to new user
-        io.emit("get-users", activeUsers);
-    });
+  // add new User
+  socket.on("new-user-add", (newUserId) => {
+    // if user is not added previously
+    if (!activeUsers.some((user) => user.userId === newUserId)) {
+      activeUsers.push({ userId: newUserId, socketId: socket.id });
+      console.log("New User Connected", activeUsers);
+    }
+    // send all active users to new user
+    io.emit("get-users", activeUsers);
+  });
 
-    socket.on("disconnect", () => {
-        // remove user from active users
-        activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
-        console.log("User Disconnected", activeUsers);
-        // send all active users to all users
-        io.emit("get-users", activeUsers);
-    });
+  socket.on("disconnect", () => {
+    // remove user from active users
+    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+    console.log("User Disconnected", activeUsers);
+    // send all active users to all users
+    io.emit("get-users", activeUsers);
+  });
 
-    // send message to a specific user
-    socket.on("send-message", (data) => {
-        const { receiverId } = data;
-        const user = activeUsers.find((user) => user.userId === receiverId);
-        console.log("Sending from socket to :", receiverId)
-        console.log("Data: ", data)
-        if (user) {
-            io.to(user.socketId).emit("recieve-message", data);
-        }
-    });
+  // send message to a specific user
+  socket.on("send-message", (data) => {
+    const receiverId = data?.receiverId;
+    const user = activeUsers.find((user) => user.userId === receiverId);
+    console.log("Sending from socket to :", receiverId);
+    console.log("Data: ", data);
+    console.log("user: ", activeUsers, "user", user);
+    if (user !== undefined) {
+      io.to(user.socketId).emit("receive-message", data);
+    }
+  });
 });
